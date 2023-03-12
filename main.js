490e8f19c0a2504fb4255392ee76f19259981efe
Sarah Oliveira <sarah.mdeo@outlook.com>
2023-03-12 17:47:31 -0300

    fixing the issue with adding a <li> into <ul>

--- main.js
+++ main.js
@@ -6,47 +6,56 @@ const taskList = document.querySelector(".task-list");
 // Array that will receive the tasks thata were added
 let taskListArray = [];
 
-// EventListener that adds a <li> to the <ul> in HTML
-addTaskBtn.addEventListener("click", addNewTask);
-addTaskBtn.addEventListener("keypress", (e) => {
-  if (e.key == "Enter") {
-    addNewTask();
-  }
-});
-
-let itemText = document.createElement("p");
-itemText.classList.add("item-text");
-let newTaskItem = document.createElement("li");
-newTaskItem.classList.add("list-item");
-
 function addNewTask() {
+  let newTaskItem = document.createElement("li");
+  newTaskItem.classList.add("list-item");
+  let itemText = document.createElement("p");
+  itemText.classList.add("item-text");
+
   if (newTaskInput.value == "") {
     alert("There's no text on the input");
   } else {
+    taskListArray.push({ taskName: newTaskInput.value, status: "unchecked" });
+    // adds <li> in the <ul>
+    taskList.appendChild(newTaskItem);
+    // creating the trash can element
     let trashCan = document.createElement("span");
     trashCan.innerHTML = `<i class="fa-solid fa-trash"></i>`;
 
-    taskList.appendChild(newTaskItem);
-
+    // adds the <p> in the <li>
     newTaskItem.appendChild(itemText);
+
+    // pust the input text into the <p>
     itemText.textContent = newTaskInput.value;
+    // appends the trash can icon into the <li>
     newTaskItem.appendChild(trashCan);
+    // erases the text in the input
     newTaskInput.value = "";
+
+    function checkItem() {
+      itemText.classList.toggle("checked");
+
+      if (itemText.classList.contains("checked")) {
+        itemText.style.textDecoration = "line-through";
+        itemText.style.color = "rgba(0,0,0,0.3)";
+        newTaskItem.style.backgroundColor = "var(--secondary-lighter)";
+      } else {
+        itemText.style.textDecoration = "none";
+        itemText.style.color = "#303030";
+        newTaskItem.style.backgroundColor = "rgba(0, 0, 0, 0.02)";
+      }
+    }
+
+    newTaskItem.addEventListener("click", checkItem);
   }
 }
 
-function checkItem() {
-  itemText.classList.toggle("checked");
+// EventListener that adds a <li> to the <ul> in HTML
+addTaskBtn.addEventListener("click", addNewTask);
 
-  if (itemText.classList.contains("checked")) {
-    itemText.style.textDecoration = "line-through";
-    itemText.style.color = "rgba(0,0,0,0.3)";
-    newTaskItem.style.backgroundColor = "var(--secondary-lighter)";
-  } else {
-    itemText.style.textDecoration = "none";
-    itemText.style.color = "#303030";
-    newTaskItem.style.backgroundColor = "rgba(0, 0, 0, 0.02)";
+// EventListener that monitors the input text, if enter is pressed, it runs de addNewTask()
+newTaskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addNewTask();
   }
-}
-
-itemText.addEventListener("click", checkItem);
+});
