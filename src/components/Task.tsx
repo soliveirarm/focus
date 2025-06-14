893477504ff65c7c2dae90574ce3ed7fda6d0160
Sarah Oliveira <soliveirarm@gmail.com>
2025-06-14 03:01:00 -0300

    fix(index.css): fix check images path

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -20,7 +20,7 @@ export function Task({
   return (
     <>
       <input
-        className="task__checkbox relative flex flex-none items-center justify-center size-5 bg-inherit appearance-none border border-accent dark:border-accent-dark rounded-full transition-all cursor-pointer hover:scale-115 checked:bg-accent dark:checked:bg-accent-dark "
+        className="task__checkbox relative flex flex-none items-center justify-center size-5 bg-inherit appearance-none border border-accent dark:border-accent-dark rounded-full transition-all cursor-pointer hover:scale-115 checked:bg-accent dark:checked:bg-accent-dark"
         type="checkbox"
         defaultChecked={done}
         onClick={toggleTaskCompletion}
